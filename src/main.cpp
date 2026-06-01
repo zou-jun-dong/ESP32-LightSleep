6370feabec29060cbc9d338f5a35eb8de96289f3
zou-jun-dong <20250070118@stu.fosu.edu.cn>
2026-06-01 12:32:10 +0800

    feat(day1): implement light sleep and verify warm boot behavior

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
 #include <Arduino.h>
+#include "esp_sleep.h"
 
-// put function declarations here:
-int myFunction(int, int);
-
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+#define us_TO_S_FACTOR 1000000ULL
+void setup()
+{
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("\n=== Day 1: Light Sleep Test ===");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  Serial.println("System is running normal active tasks...");
+  delay(1000);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("Entering Light Sleep for 5 seconds...(Peripherals & retained)");
+  Serial.flush();  //Ensure UART buffer is empty before sleeping
+
+  //Configure Timer for Light Sleep
+  esp_sleep_enable_timer_wakeup(5*us_TO_S_FACTOR);
+  //Start Light sleep.CPU pauses,but RAM is kept alive
+  esp_light_sleep_start();
+
+  //[Verification] if it is a Warm Boot,execution continues right here
+  Serial.println("Woken up from Light Sleep! (Warm Boot Success)");
+  Serial.println("Notice that setup() was NOT caller again.\n");
+
+  delay(2000);
 }
